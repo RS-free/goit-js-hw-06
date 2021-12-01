992096a7310d87b9ca2bc1d53595eeb4608fb721
Ruslan <ruslansulieim@gmail.com>
2021-12-01 16:51:27 +0200

    1, 2

---
+++ src/js/task-01.js
@@ -0,0 +1,9 @@
+const mainList = document.querySelector("ul");
+console.log(mainList);
+const listItems = mainList.querySelectorAll(".item");
+console.log(`Number of categories: ${listItems.length}`);
+const h2Name = mainList.getElementsByTagName("h2");
+listItems.forEach((element) => {
+  console.log("Category:", element.getElementsByTagName("h2")[0].textContent);
+  console.log("Elements:", element.getElementsByTagName("li").length);
+});
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,16 @@
+const ingredList = document.querySelector("#ingredients");
+console.log(ingredList);
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const newItems = ingredients.map((element) => {
+  const newItem = document.createElement("li");
+  newItem.textContent = element;
+  return newItem;
+});
+ingredList.append(...newItems);
