b2c27bb0961db79a9f007ba73fa4d7b4a8d9f6e4
Ruslan <ruslansulieim@gmail.com>
2021-12-05 23:02:50 +0200

    8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
-
+const form = document.querySelector(".login-form");
+let message = "";
+form.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+        mail,
+        password,
+    };
+    if (mail === "" || password === "") {
+        message = alert("All fields must be completed");
+    } else {
+        form.reset();
+        console.log(formData);
+    }
+}
