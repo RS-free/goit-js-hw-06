150eece38b778e03ec422046b553a048bc7f23dd
Ruslan <ruslansulieim@gmail.com>
2021-12-08 12:28:47 +0200

    7

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const inputSizeControl = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
+text.style.fontSize = `${inputSizeControl.value}px`;
 
 inputSizeControl.addEventListener("input", function () {
     text.style.fontSize = `${inputSizeControl.value}px`;
